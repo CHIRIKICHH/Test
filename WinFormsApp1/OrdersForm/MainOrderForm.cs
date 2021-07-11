5e7c24addc3c38ea89358b17c6ce919ddca00b35
CHIRIKICHH <nikolay.omelchenko.03@gmail.com>
2021-07-11 13:57:40 +0300

    deleted new Form1

--- WinFormsApp1/OrdersForm/MainOrderForm.cs
+++ WinFormsApp1/OrdersForm/MainOrderForm.cs
@@ -25,7 +25,7 @@ namespace WinFormsApp1
             dataGridView1.Columns[5].Width = 100;
             dataGridView1.Columns[7].Width = 60;
 
-            MessageBox.Show("Абоба");
+            new Form1().Show();
         }
 
         //При загрузке формы чтение из БД
